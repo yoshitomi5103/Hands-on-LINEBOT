2b2520d80100787266020955e24a06cd38e0ef18
shinbunbun <takamuneyuto@gmail.com>
2022-08-22 10:08:36 +0000

    add 天気予報

--- src/event/message/text_map.js
+++ src/event/message/text_map.js
@@ -1,3 +1,5 @@
+import { get } from '../../request.js';
+
 // ユーザーのプロフィールを取得する関数
 const getUserProfile = (event, client) => client.getProfile(event.source.userId);
 
@@ -368,4 +370,18 @@ export const messageMap = {
     type: 'text',
     text: `ここは${event.source.type}だよ！`,
   }),
+  天気予報: async () => {
+    // axiosを使ってAPIにGETリクエストを送り、レスポンスのdataを変数resに格納
+    const weatherApiRes = (await get('https://www.jma.go.jp/bosai/forecast/data/forecast/070000.json')).data;
+    // 返信するメッセージを作成
+    return {
+      type: 'text',
+      text: `【天気予報】
+    
+    ${weatherApiRes[0].timeSeries[0].timeDefines[0]}: ${weatherApiRes[0].timeSeries[0].areas[2].weathers[0]}
+    ${weatherApiRes[0].timeSeries[0].timeDefines[1]}: ${weatherApiRes[0].timeSeries[0].areas[2].weathers[1]}
+    ${weatherApiRes[0].timeSeries[0].timeDefines[2]}: ${weatherApiRes[0].timeSeries[0].areas[2].weathers[2]} 
+    `,
+    };
+  },
 };
