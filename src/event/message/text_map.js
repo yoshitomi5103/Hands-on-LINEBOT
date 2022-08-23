4f633461e160bf3102f681f3025019091af8c44c
shinbunbun <takamuneyuto@gmail.com>
2022-08-23 11:25:54 +0900

    add ex4-2-1

--- src/event/message/text_map.js
+++ src/event/message/text_map.js
@@ -532,4 +532,13 @@ export const messageMap = {
     });
     return message;
   },
+  蔵書検索: async (event, appContext) => {
+    // DBにコンテキストを追加
+    await createData(event.source.userId, 'context', 'bookSearchMode', appContext);
+
+    return {
+      type: 'text',
+      text: '検索したい本のISBNを送ってください\n※本のISBNは「検索したい本のタイトル ISBN」などとGoogleで検索すれば取得できます',
+    };
+  },
 };
